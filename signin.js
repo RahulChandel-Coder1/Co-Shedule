8df92a993fcf7eac0bb4bdcb3d54427641954f22
Adarsh <singhmahan988@gmail.com>
2022-07-21 23:39:39 +0530

    updated

---
+++ signin.js
@@ -0,0 +1,30 @@
+
+
+var localemail;
+var localpass;
+window.onload = function () {
+    var form = document.getElementById("form1");
+    form.addEventListener("submit", formSignin);
+   
+    var user = localStorage.getItem("localuser");
+    user = JSON.parse(user);
+    localemail = user[0].email;
+    localpass = user[0].password;
+
+}
+
+function formSignin(e) {
+    e.preventDefault()
+    var email1 = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    var errormsg1 = document.getElementById("errormsg");
+    errormsg1.innerHTML = "";
+    console.log(email1, pass)
+    if(email1 == localemail && localpass == pass) {
+        // Comment====>USE home page link here
+          location.assign("signUp.html")   
+           
+    }else {
+        errormsg1.innerHTML = "incorrect email or password"
+    }
+}
